355c7df96871cf9b3956bc6becf9189e91428dae
Gabriel Cardona <gabriel.cardona@bluerocket.us>
2017-09-09 17:25:01 -0700

    title

--- src/components/Html.js
+++ src/components/Html.js
@@ -41,7 +41,7 @@ class Html extends React.Component {
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <title>BitcoinCash.EARTH - Social Philanthropy</title>
+          <title>Social Philanthropy - BitcoinCash.EARTH</title>
           <meta name="description" content={description} />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           {scripts.map(script =>
